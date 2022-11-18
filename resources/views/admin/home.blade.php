3753dd28b31ca34fd629c574a2aaa04bd97c8ff0
Tanzina Akter <dali0023@algonquinlive.com>
2022-11-18 08:32:37 -0500

    setup layout and design for both users and admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,15 +1,30 @@
-<x-app-layout>
-
-</x-app-layout>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Admin Dashboard</h1>
-</body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Shopping Hub- Admin</title>
+    <!-- plugins:css -->
+    @include('admin.style')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_navbar.html -->
+      @include('admin.navbar')
+
+        <!-- partial -->
+      @include('admin.body')
+
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    @include('admin.script')
+  </body>
 </html>
